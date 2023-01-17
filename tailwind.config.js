ca08c0888f8f1ffc693f3ea686740179d47dc06f
fatimakhosravi <fateme.khosravi3307@gmail.com>
2023-01-17 20:05:30 +0330

    add new theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwindcss-rtl'),
+  ],
+  theme: {
+    colors: {
+      "costume-green": "#00C853",
+      "costume-red": "#FE5656",
+      "costume-black": "#000000",
+      "costume-gray": "#333333"
+    },
+  }
+};
+
